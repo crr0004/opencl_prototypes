93114f6b202ad7e5e88f5c23e0d0be3bc2eddc77
Christopher Rhodes <P784670@aur.national.com.au>
2019-06-05 11:44:07 +1000

    Fix error checking to be one line

--- first_try/main.cpp
+++ first_try/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+// Silence
+#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
 #else
@@ -82,10 +86,7 @@ int main(void) {
 	fprintf(stdout, "Using %d Platform name %s.\n", i, outBuf);
 	printDeviceInfo(device_id);
  
-    if(ret != CL_SUCCESS){
-		clCheckError(ret);
-		exit(1);
-	}
+	clCheckError(ret);
 
 
 
@@ -106,16 +107,11 @@ int main(void) {
     // Copy the lists A and B to their respective memory buffers
     ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
             LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
-    if(ret != CL_SUCCESS){
-		clCheckError(ret);
-		exit(1);
-	}
+	clCheckError(ret);
+
     ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
             LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
-    if(ret != CL_SUCCESS){
-		clCheckError(ret);
-		exit(1);
-	}
+	clCheckError(ret);
  
     // Create a program from the kernel source
     cl_program program = clCreateProgramWithSource(context, 1, 
@@ -123,10 +119,7 @@ int main(void) {
  
     // Build the program
     ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-    if(ret != CL_SUCCESS){
-		clCheckError(ret);
-		exit(1);
-	}
+	clCheckError(ret);
  
     // Create the OpenCL kernel
     cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
@@ -135,29 +128,20 @@ int main(void) {
     ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
     ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
     ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
-    if(ret != CL_SUCCESS){
-		clCheckError(ret);
-		exit(1);
-	}
+	clCheckError(ret);
  
     // Execute the OpenCL kernel on the list
     size_t global_item_size = LIST_SIZE; // Process the entire lists
     size_t local_item_size = 64; // Divide work items into groups of 64
     ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
             &global_item_size, &local_item_size, 0, NULL, NULL);
-    if(ret != CL_SUCCESS){
-		clCheckError(ret);
-		exit(1);
-	}
+	clCheckError(ret);
  
     // Read the memory buffer C on the device to the local variable C
     int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
     ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 
             LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
-    if(ret != CL_SUCCESS){
-		clCheckError(ret);
-		exit(1);
-	}
+	clCheckError(ret);
  
     // Display the result to the screen
     for(i = 0; i < LIST_SIZE; i++)
