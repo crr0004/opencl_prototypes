0825a50377dae3ea7ff9d0db3779f052d4106b94
crr0004 <crr0004@gmail.com>
2019-07-20 16:12:55 +1000

    Add better error checking

--- first_try/kernel.cl
+++ first_try/kernel.cl
@@ -1,8 +1,10 @@
 __kernel void vector_add(__global const int *A, __global const int *B, __global int *C) {
  
     // Get the index of the current element to be processed
-    int i = get_global_id(0);
+    int i = get_global_id(1);
  
     // Do the operation
     C[i] = A[i] + B[i];
+//	C[i] = C[i] - 1;
+	C[i] = 10;
 }
--- first_try/main.cpp
+++ first_try/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
  
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
@@ -9,6 +10,13 @@
 #include "clext.h"
  
 #define MAX_SOURCE_SIZE (0x100000)
+
+void printPlatformName(cl_platform_id id){
+	char outBuf[256];
+	size_t outBufWritten = 0;
+	clGetPlatformInfo(id, CL_PLATFORM_NAME, 256, outBuf, &outBufWritten);
+	fprintf(stdout, "Platform name %s.\n", outBuf);
+}
  
 int main(void) {
     // Create the two input vectors
@@ -36,13 +44,24 @@ int main(void) {
     fclose( fp );
  
     // Get platform and device information
-    cl_platform_id platform_id = NULL;
+    cl_platform_id platform_id[2];
     cl_device_id device_id = NULL;   
     cl_uint ret_num_devices;
     cl_uint ret_num_platforms;
-    cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-    ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_DEFAULT, 1, 
+    cl_int ret = clGetPlatformIDs(2, platform_id, &ret_num_platforms);
+    ret = clGetDeviceIDs( platform_id[0], CL_DEVICE_TYPE_DEFAULT, 1, 
             &device_id, &ret_num_devices);
+
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
+
+	char buf[128];
+	clGetDeviceInfo(device_id, CL_DEVICE_NAME, 128, buf, NULL);
+	fprintf(stdout, "Device %s supports.\n", buf);
+
+	printPlatformName(platform_id[0]);
  
     if(ret != CL_SUCCESS){
 		clCheckError(ret);
@@ -50,17 +69,48 @@ int main(void) {
 	}
     // Create an OpenCL context
     cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
+
+	cl_device_id used_device_id;
+	ret = clGetContextInfo(context, CL_CONTEXT_DEVICES, sizeof used_device_id, &used_device_id, NULL);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
+	std::memset(buf, 0, sizeof buf);
+	clGetDeviceInfo(device_id, CL_DEVICE_NAME, 128, buf, NULL);
+	fprintf(stdout, "Using %s for context device.\n", buf);
+	
  
     // Create a command queue
     cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
  
     // Create memory buffers on the device for each vector 
     cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
             LIST_SIZE * sizeof(int), NULL, &ret);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
     cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
             LIST_SIZE * sizeof(int), NULL, &ret);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
     cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
             LIST_SIZE * sizeof(int), NULL, &ret);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
  
     // Copy the lists A and B to their respective memory buffers
     ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
@@ -79,6 +129,10 @@ int main(void) {
     // Create a program from the kernel source
     cl_program program = clCreateProgramWithSource(context, 1, 
             (const char **)&source_str, (const size_t *)&source_size, &ret);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
  
     // Build the program
     ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
@@ -89,10 +143,22 @@ int main(void) {
  
     // Create the OpenCL kernel
     cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
  
     // Set the arguments of the kernel
     ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
     ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+    if(ret != CL_SUCCESS){
+		clCheckError(ret);
+		exit(1);
+	}
     ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
     if(ret != CL_SUCCESS){
 		clCheckError(ret);
@@ -101,7 +167,7 @@ int main(void) {
  
     // Execute the OpenCL kernel on the list
     size_t global_item_size = LIST_SIZE; // Process the entire lists
-    size_t local_item_size = 64; // Divide work items into groups of 64
+    size_t local_item_size = 32; // Divide work items into groups of 64
     ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
             &global_item_size, &local_item_size, 0, NULL, NULL);
     if(ret != CL_SUCCESS){
